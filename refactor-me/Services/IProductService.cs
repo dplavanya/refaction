3719896b565a591b6c628a6ddd0f87d26b052e46
Durga Mallampalli <durgaprasad.mallampalli@gmail.com>
2017-07-05 15:59:08 +1200

    Added Service Layer

---
+++ refactor-me/Services/IProductService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using refactor_me.Models;
+
+namespace refactor_me.Services
+{
+    public interface IProductService
+    {
+        Task<IList<Product>> GetAllProductsAsync();
+
+        Task<Product> GetProductByIdAsync(Guid id);
+
+        Task<IList<Product>> SearchProductsByNameAsync(string name);
+
+        Task<Product> CreateProductAsync(Product product);
+
+        Task<Product> UpdateProductAsync(Guid id, Product product);
+
+        Task<Product> DeleteProductAsync(Guid id);
+
+
+    }
+}
---
+++ refactor-me/Services/ProductService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using refactor_me.Models;
+
+namespace refactor_me.Services
+{
+    public class ProductService : IProductService
+    {
+        public Task<Product> CreateProductAsync(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> DeleteProductAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<Product>> GetAllProductsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetProductByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<Product>> SearchProductsByNameAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> UpdateProductAsync(Guid id, Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
