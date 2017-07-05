7ca934f7b295636b2b78486f6d59851575c3152b
Durga Mallampalli <durgaprasad.mallampalli@gmail.com>
2017-07-05 16:14:00 +1200

    Created Separate Libraries for Contracts & Repositories

---
+++ Refactorme.Repository.Contracts/IProductRepository.cs
@@ -0,0 +1,24 @@
+﻿using Refactoreme.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refactorme.Repository.Contracts
+{
+    public interface IProductRepository
+    {
+        Task<IList<Product>> GetAllProductsAsync();
+
+        Task<Product> GetProductByIdAsync(Guid id);
+
+        Task<IList<Product>> SearchProductsByNameAsync(string name);
+
+        Task<Product> CreateProductAsync(Product product);
+
+        Task<Product> UpdateProductAsync(Guid id, Product product);
+
+        Task<Product> DeleteProductAsync(Guid id);
+    }
+}
---
+++ Refactorme.Repository/ProductRepository.cs
@@ -0,0 +1,43 @@
+﻿using Refactorme.Repository.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Refactoreme.Data.Models;
+
+namespace Refactorme.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        public Task<IList<Product>> GetAllProductsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetProductByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<Product>> SearchProductsByNameAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> DeleteProductAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> UpdateProductAsync(Guid id, Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> CreateProductAsync(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
