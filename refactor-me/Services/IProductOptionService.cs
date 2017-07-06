9e4f3470f72cc79b5176a9ed0c95fa1aa6ec98ff
Durga Mallampalli <durgaprasad.mallampalli@gmail.com>
2017-07-06 13:18:50 +1200

    Added ProductOption Serivce

---
+++ refactor-me/Services/IProductOptionService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Refactoreme.Data.Models;
+
+namespace refactor_me.Services
+{
+    public interface IProductOptionService
+    {
+        Task<IList<ProductOption>> GetAllProductOptionsByProductIdAsync(Guid productId);
+
+        Task<ProductOption> GetProductOptionByOptionIdAsync(Guid productId, Guid optionId);
+
+        Task<ProductOption> UpdateProductOptionAsync(ProductOption productOption);
+
+        Task<bool> DeleteProductOptionAsync(Guid productId, Guid optionId);
+    }
+}
---
+++ refactor-me/Services/ProductOptionService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Refactoreme.Data.Models;
+
+namespace refactor_me.Services
+{
+    public class ProductOptionService : IProductOptionService
+    {
+        public Task<IList<ProductOption>> GetAllProductOptionsByProductIdAsync(Guid productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProductOption> GetProductOptionByOptionIdAsync(Guid productId, Guid optionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProductOption> UpdateProductOptionAsync(ProductOption productOption)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteProductOptionAsync(Guid productId, Guid optionId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
